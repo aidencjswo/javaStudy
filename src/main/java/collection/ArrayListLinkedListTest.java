8328750a3558fdae70d4416dc3556ed7bf670502
aidencjswo@gmail.com <aidencjswo@gmail.com>
2023-03-16 01:09:59 +0900

    LinkedList까지

---
+++ src/main/java/collection/ArrayListLinkedListTest.java
@@ -0,0 +1,9 @@
+package collection;
+
+public class ArrayListLinkedListTest {
+    public static void main(String[] args) {
+        /**
+         * 구현해보기
+         */
+    }
+}
---
+++ src/main/java/collection/LikedListTest.java
@@ -0,0 +1,29 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class LikedListTest {
+    public void test(){
+        /**
+         * 1. LinkedList
+         * Node *
+         * next : Object -> 다음 요소의 참조주소를 저장
+         */
+
+        /**
+         * 2. doublyLinkedList
+         * Node *
+         * next : Object -> 다음 요소의 참조주소를 저장
+         * previous : Object -> 이전 요소의 참조주소를 저장
+         */
+
+        /**
+         * 3. doublyCircularLinkedList
+         * Node *
+         * next : Object -> 다음 요소의 참조주소를 저장
+         * previous : Object -> 이전 요소의 참조주소를 저장
+         * first : Object -> 마지막 요소의 참조주소를 저장
+         * last : Object -> 첫번째 요소의 참조주소를 저장
+         */
+    }
+}
